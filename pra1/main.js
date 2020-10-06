2e9d38b63b4e3cc988b93fae9a3089d12faa5e54
marnysan111 <o-masato@outlook.jp>
2020-10-06 10:54:38 +0900

    10/06

---
+++ pra1/main.js
@@ -0,0 +1,81 @@
+new Vue({
+    el: '#app',
+    data: {
+        Message: 'Hello'
+    },
+    computed: {
+        reverseMessage: function() {
+            return this.Message.split('').reverse().join('')
+        }
+    }
+})
+
+new Vue({
+    el: '#app2',
+    data: {
+      firstName: 'Foo',
+      lastName: 'Bar'
+    },
+    computed: {
+        fullName: {
+            // getter 関数
+            get: function () {
+              return this.firstName + ' ' + this.lastName
+            },
+            // setter 関数
+            set: function (newValue) {
+              var names = newValue.split(' ')
+              this.firstName = names[0]
+              this.lastName = names[names.length - 1]
+            }
+          }
+    }
+  })
+
+  var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+      question: '',
+      answer: 'I cannot give you an anser until you ask a question!'
+    },
+    watch: {
+      // この関数は question が変わるごとに実行されます。
+      question: function (newQuestion, oldQuestion) {
+        this.answer = 'Waitting gor you to stop typing...'
+        this.debouncedGetAnswer()
+      }
+    },
+    created: function () {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+      getAnswer: function () {
+        if (this.question.indexOf('?') === -1 ) {
+          this.answer = 'Question usually contain a question mark. :-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' * error
+        })
+      }
+    }
+  })
+
+  new Vue({
+    el: '#app3',
+    // ここから追加 ②
+    data: {
+      classObject: {
+        active: true,
+      },
+      styleObject: {
+        fontSize: '50px',
+      }
+    },
+  })
